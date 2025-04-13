f7e2b33134bab3c61c6f4469ac222a70385fc1ed
iteamfox <avlisunenko@gmail.com>
2025-04-13 15:01:46 +0200

    ternary operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,6 +2,7 @@ export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
   const partner = sex === 'm' ? 'wife' : 'husband';
+
   const partnerStatus = isMarried
     ? `${partnerName} is my ${partner}`
     : 'I am not married';
